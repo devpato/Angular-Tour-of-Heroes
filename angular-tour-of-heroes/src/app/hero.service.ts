4d4f2af333e62d629c3038ec48ab901c2b5f5b02
pevargasg <tamo@wearetamo.com>
2018-08-22 01:27:31 -0400

    get objects

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Hero } from "./hero.module";
 import { HEROES } from "./mock-heroes";
+import { Observable } from "rxjs/Observable";
+import { of } from "rxjs/observable/of";
 
 @Injectable()
 export class HeroService {
   constructor() {}
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
