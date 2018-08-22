d569582971bd0ff67979814fa6dc4f36a442d834
pevargasg <tamo@wearetamo.com>
2018-08-22 01:20:19 -0400

    getters in service

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Hero } from "./hero.module";
+import { HEROES } from "./mock-heroes";
 
 @Injectable()
 export class HeroService {
+  constructor() {}
 
-  constructor() { }
-
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }
