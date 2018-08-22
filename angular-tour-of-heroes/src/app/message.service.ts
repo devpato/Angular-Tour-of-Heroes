8c796f0b753a352f3edd808fd9f098a07457053f
pevargasg <tamo@wearetamo.com>
2018-08-22 01:32:00 -0400

    message service implemented

--- angular-tour-of-heroes/src/app/message.service.ts
+++ angular-tour-of-heroes/src/app/message.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class MessageService {
+  messages: string[] = [];
 
-  constructor() { }
+  add(message: string) {
+    this.messages.push(message);
+  }
 
+  clear() {
+    this.messages = [];
+  }
 }
